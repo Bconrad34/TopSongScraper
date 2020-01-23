b35cef0395a16eac3634e9b293303c48c275f8ef
Bconrad34 <bconrad1217@gmail.com>
2020-01-22 20:56:27 -0500

    initial commit

---
+++ BillboardScraper.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package billboardscraper;
+
+import my.billboardscraper.BillboardScraperGUI;
+
+/**
+ *
+ * @author bconr
+ */
+public class BillboardScraper {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        BillboardScraperGUI form = new BillboardScraperGUI();
+        form.setVisible(true);
+    }
+    
+}
